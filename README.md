f32b91de5fb72f2466889508b0c3898982c2f24a
trudly <trudly@outlook.com>
2017-08-30 18:05:38 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# test-python #
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+
+[![Deploy to Bluemix](https://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/trudly/test-python)
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import request
+from flask import Response
+from pyzceqsolver import Solver
+import subprocess
+import string
+import array
+import binascii
+import hashlib
+import json
+from ctypes import *
+
+lib = cdll.LoadLibrary('./libpure.so')
+lib.py_result.restype = c_char_p
+py_result = lib.py_result
+py_result.restype = c_char_p
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def qqruqu():
+    out = py_result("0505f49384cb054b16f69fc108640a8f627e268f51d897b4f7c74eafe9777fefd9b04abed9eec500000000e321bd84e6b2008879a16497bf6ee58644a36146b63de7c217e7f467b05d67fa01", 160, 170)
+    return out
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello from Flask!?'
